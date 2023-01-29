77fbef35c5e72fb5282c49bc74dbfa0c051763e1
yao jing quek <qyaojing@gmail.com>
2023-01-29 15:29:35 +0800

    remove chipyard submodule

--- run_container.sh
+++ run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./clean_container.sh
-sudo docker run --name chipyard -v $(pwd)/chipyard:/root/mount/chipyard -it ucbbar/chipyard-image bash
+sudo docker run --name chipyard -v $(pwd):/root/mount/ -it ucbbar/chipyard-image bash
